f971eb4cc1235d3b46706f62a2dd99d45cbe9072
ERLMKea <erlm@kea.dk>
2022-08-28 23:03:15 +0200

    Initial commit

---
+++ src/main/java/com/example/workflow02/Workflow02Application.java
@@ -0,0 +1,13 @@
+package com.example.workflow02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Workflow02Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Workflow02Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/workflow02/Workflow02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.workflow02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Workflow02ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
